7cdea848148ddf42c6fa7c228cc24bd6890fe009
srprompt <75858528+srprompt@users.noreply.github.com>
2020-12-16 12:42:48 -0300

    Update Ctype.h.c

--- Ctype.h.c
+++ Ctype.h.c
@@ -9,20 +9,20 @@ int main()
 
     int i;
 
-    //FUN«√O GR¡FICO E ALFANUM…RICA
-    printf("\t||##### GR¡FICO E ALFANUM #####||\n\n");
-    printf("Em linguagem C, todos os caracteres "     //Exemplo utilizando os n˙meros da tabela ASCII.
-           "gr·ficos s„o: \n\n");
+    //FUN√á√ÉO GR√ÅFICO E ALFANUM√âRICA
+    printf("\t||##### GR√ÅFICO E ALFANUM #####||\n\n");
+    printf("Em linguagem C, todos os caracteres "     //Exemplo utilizando os n√∫meros da tabela ASCII.
+           "gr√°ficos s√£o: \n\n");
 
     for (i = 0; i <= 127; ++i)   //Percorre os 127 caracteres da tabela ASCII.
-        if (isgraph(i) != 0)     //Todas as funÁıes retornam 0 (zero) caso a comparaÁ„o n„o seja verdadeira.
-            printf("%c ", i);    //Cada n˙mero representa um caractere na tabela ASCII, neste caso È mostrado
-    //de i=33 que equivale ao ! atÈ o i=126 que equivale ao ~ .
+        if (isgraph(i) != 0)     //Todas as fun√ß√µes retornam 0 (zero) caso a compara√ß√£o n√£o seja verdadeira.
+            printf("%c ", i);    //Cada n√∫mero representa um caractere na tabela ASCII, neste caso √© mostrado
+    //de i=33 que equivale ao ! at√© o i=126 que equivale ao ~ .
 
-    printf("\n\nAlfanumÈricos: \n\n");
+    printf("\n\nAlfanum√©ricos: \n\n");
 
     for (i = 0; i <= 127; ++i)
-        if (isalnum(i) != 0)    //O mesmo exemplo porÈm agora mostrando somente os alfanumÈricos.
+        if (isalnum(i) != 0)    //O mesmo exemplo por√©m agora mostrando somente os alfanum√©ricos.
             printf("%c ", i);
 
     printf("\n");
@@ -30,65 +30,65 @@ int main()
     printf("\n----------------------------------------------------------------------------------------\n");
 
     char c;
-    int exemplo_retorno;       //Exemplo demonstrando a difirenÁa nos valores de retorno das funÁıes da biblioteca ctype.h
+    int exemplo_retorno;       //Exemplo demonstrando a difiren√ßa nos valores de retorno das fun√ß√µes da biblioteca ctype.h
 
-    //FUN«√O CONTROLE
+    //FUN√á√ÉO CONTROLE
     printf("\t||##### CONTROLE #####||\n\n");
 
     c = 'Q';
     exemplo_retorno = iscntrl(c);
-    printf("Quando %c È passado o iscntrl() = %d\n", c, exemplo_retorno);   //Mostra que quando a verifica o caractere caso n„o
+    printf("Quando %c √© passado o iscntrl() = %d\n", c, exemplo_retorno);   //Mostra que quando a verifica o caractere caso n√£o
     //seja do tipo comparado retorna o valor 0.
 
     c = '\n';
     exemplo_retorno = iscntrl(c);
-    printf("Quando 'quebra de linha' È passado o iscntrl() = %d", c, exemplo_retorno);   //Caso seja verdadeiro retorna um valor diferente de 0.
+    printf("Quando 'quebra de linha' √© passado o iscntrl() = %d", c, exemplo_retorno);   //Caso seja verdadeiro retorna um valor diferente de 0.
 
     printf("\n----------------------------------------------------------------------------------------\n");
 
-    char exemplo[]="3x3mpL0...3x3mpL0\n";   //… possivel com essas funÁıes separar e mostrar cada tipo de caractere.
+    char exemplo[]="3x3mpL0...3x3mpL0\n";   //√â possivel com essas fun√ß√µes separar e mostrar cada tipo de caractere.
 
-    //ALFANUM, LETRAS, MAIUSC/MINUSC, IMPRIME, PONTUA«√O E DÕGITOS
-    printf("\t||##### VERIFICA ALFANUM, LETRAS, MAIUSC/MINUSC, IMPRIME, PONTUA«√O E DÕGITOS #####||\n\n");
+    //ALFANUM, LETRAS, MAIUSC/MINUSC, IMPRIME, PONTUA√á√ÉO E D√çGITOS
+    printf("\t||##### VERIFICA ALFANUM, LETRAS, MAIUSC/MINUSC, IMPRIME, PONTUA√á√ÉO E D√çGITOS #####||\n\n");
 
     for(i=0; i<19; i++)
-        if(isalnum(exemplo[i]))             //Verifica se os caracteres s„o alfanumericos.
+        if(isalnum(exemplo[i]))             //Verifica se os caracteres s√£o alfanumericos.
             printf ("%c ",exemplo[i]);
 
     printf("\n\n");
 
     for(i=0; i<19; i++)
-        if(isalpha(exemplo[i]))             //Verifica se s„o letras.
+        if(isalpha(exemplo[i]))             //Verifica se s√£o letras.
             printf ("%c ",exemplo[i]);
 
     printf("\n\n");
 
     for(i=0; i<19; i++)
-        if(islower(exemplo[i]))             //Verifica se s„o letras min˙sculas
+        if(islower(exemplo[i]))             //Verifica se s√£o letras min√∫sculas
             printf ("%c ",exemplo[i]);
 
     printf("\n\n");
 
     for(i=0; i<19; i++)
-        if(isprint(exemplo[i]))             //Verifica se s„o caracteres imprimiveis.
+        if(isprint(exemplo[i]))             //Verifica se s√£o caracteres imprimiveis.
             printf ("%c ",exemplo[i]);
 
     printf("\n\n");
 
     for(i=0; i<19; i++)
-        if(ispunct(exemplo[i]))             //Verifica se s„o caracteres de pontuaÁ„o.
+        if(ispunct(exemplo[i]))             //Verifica se s√£o caracteres de pontua√ß√£o.
             printf ("%c ",exemplo[i]);
 
     printf("\n\n");
 
     for(i=0; i<19; i++)
-        if(isupper(exemplo[i]))             //Verifica se s„o letras mai˙sculas.
+        if(isupper(exemplo[i]))             //Verifica se s√£o letras mai√∫sculas.
             printf ("%c ",exemplo[i]);
 
     printf("\n\n");
 
     for(i=0; i<19; i++)
-        if(isdigit(exemplo[i]))             //Verifica se os caracteres s„o dÌgitos decimais.
+        if(isdigit(exemplo[i]))             //Verifica se os caracteres s√£o d√≠gitos decimais.
             printf ("%c ",exemplo[i]);
 
     printf("\n");
@@ -103,8 +103,8 @@ int main()
     exem_minuscula = 'a';
     exem_maiuscula = 'B';
 
-    exem_minuscula = toupper(exem_minuscula);  //Converte o caracter em mai˙sculo
-    exem_maiuscula = tolower(exem_maiuscula);  //Converte o caracter em min˙sculo
+    exem_minuscula = toupper(exem_minuscula);  //Converte o caracter em mai√∫sculo
+    exem_maiuscula = tolower(exem_maiuscula);  //Converte o caracter em min√∫sculo
     printf("%c %c\n", exem_minuscula, exem_maiuscula);
 
     exem_minuscula = tolower(exem_minuscula);
@@ -115,18 +115,18 @@ int main()
 
     char d;
     int y=0;
-    char str_exem[]="Testando a funÁ„o isspace\n";
+    char str_exem[]="Testando a fun√ß√£o isspace\n";
 
-    //FUN«√O ISSPACE
+    //FUN√á√ÉO ISSPACE
     //printf("\t||##### ISSPACE #####||\n\n");
 
     while (str_exem[y])
     {
-        d=str_exem[y]; //O caractere 'c' serve como auxiliar para fazer o teste dos caracteres recebidos da str_exem
-        if(isspace(d)) //Verifica se o caractere 'c' È espaÁo
-            d='\n'; //Faz 'c' receber uma quebra de linha caso seja um espaÁo
-        putchar(d); //Coloca na tela o caractere 'c'
-        y++; //Passa para a prÛxima letra da string
+        d=str_exem[y]; //O caractere 'd' serve como auxiliar para fazer o teste dos caracteres recebidos da str_exem
+        if(isspace(d)) //Verifica se o caractere 'd' √© espa√ßo
+            d='\n'; //Faz 'd' receber uma quebra de linha caso seja um espa√ßo
+        putchar(d); //Coloca na tela o caractere 'd'
+        y++; //Passa para a pr√≥xima letra da string
     }
     printf("\n----------------------------------------------------------------------------------------\n");
 
@@ -135,11 +135,11 @@ int main()
     //HEXADECIMAL
     printf("\t||##### HEXADECIMAL #####||\n\n");
 
-    str_exem1 = '2';        //Ser· considerado como um caractere hexadecimal.
-    str_exem2 = 'a';        //Ser· considerado como um caractere hexadecimal.
-    str_exem3 = 'x';        //N„o ser· considerado como um caractere hexadecimal.
+    str_exem1 = '2';        //Ser√° considerado como um caractere hexadecimal.
+    str_exem2 = 'a';        //Ser√° considerado como um caractere hexadecimal.
+    str_exem3 = 'x';        //N√£o ser√° considerado como um caractere hexadecimal.
 
-    if (isxdigit(str_exem1))                            //… feita a verificaÁ„o de todos os exemplos.
+    if (isxdigit(str_exem1))                            //√â feita a verifica√ß√£o de todos os exemplos.
         printf("%c e hexadecimal.\n", str_exem1);
     else
         printf("%c nao e hexadecimal.\n", str_exem1);
@@ -156,14 +156,14 @@ int main()
 
     printf("\n----------------------------------------------------------------------------------------\n");
 
-    //EXEMPLO PR¡TICO
+    //EXEMPLO PR√ÅTICO
     printf("\t||##### EXEMPLO SENHA #####||\n\n");
     int aux_nume = 0, aux_maiuscula = 0, aux_minuscula = 0, aux_pont = 0;
     char senha[25];
 
     while(aux_maiuscula==0 || aux_minuscula==0 || aux_nume==0 || aux_pont==0)
     {
-        printf("Digite a nova senha (Deve conter uma letra mai˙scula, uma min˙scula, um n˙mero e um caractere especial: ");
+        printf("Digite a nova senha (Deve conter uma letra mai√∫scula, uma min√∫scula, um n√∫mero e um caractere especial: ");
         fgets(senha,25,stdin);
 
         if (aux_nume == 0)
@@ -191,9 +191,9 @@ int main()
                     aux_pont = 1;
         }
         if(aux_maiuscula==1 && aux_minuscula==1 && aux_nume==1 && aux_pont==1)
-            printf("\nSenha v·lida.\n");
+            printf("\nSenha v√°lida.\n");
         else if(aux_maiuscula==0 || aux_minuscula==0 || aux_nume==0 || aux_pont==0)
-            printf("\n\nSenha inv·lida. Digite novamente.\n\n");
+            printf("\n\nSenha inv√°lida. Digite novamente.\n\n");
     }
 
     return 0;
